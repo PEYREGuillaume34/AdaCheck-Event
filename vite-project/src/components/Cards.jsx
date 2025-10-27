ffb78357192d56a46d9b9c5bad14375bbc99112e
AquaPulco <iris.yolal@gmail.com>
2025-10-27 18:26:10 +0100

    ADD: tailwind test

--- vite-project/src/components/Cards.jsx
+++ vite-project/src/components/Cards.jsx
@@ -27,9 +27,12 @@ export default function Cards() {
     <>
       <div>
         {card.map((el) => (
-          <div key={el.event_id}>
-            <h2>{el.title}</h2>
-            <div dangerouslySetInnerHTML={{ __html: el.description }}></div>
+          <div class= "gap-10 p-1" key={el.event_id}>
+
+            <h2 class="m-1 inline-block flex-center w-100 bg-blue-500 text-white rounded-xl" >{el.title}</h2>
+            <p>{el.lead_text}</p>
+            {/* <div dangerouslySetInnerHTML={{ __html: el.description }}></div> */}
+            <br></br>
           </div>
         ))}
       </div>
