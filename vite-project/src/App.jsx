9c7d156f92c94cdb7ff706d1144648aad5d5db1f
AquaPulco <iris.yolal@gmail.com>
2025-11-04 16:34:01 +0100

    commentaires code

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,18 +6,21 @@ import SearchBar from "./components/SearchBar";
 
 function App() {
   const limit = 5; // nombre d'event par page
-  const [page, setPage] = useState(1); //
-  const [offset, setOffset] = useState(0);
-  const [query, setQuery] = useState("");
-  const [totalResults, setTotalResults] = useState(0);
+  const [page, setPage] = useState(1); // page affichée
+  const [offset, setOffset] = useState(0); // nombre d'events "sautés" à l'affichage
+  const [query, setQuery] = useState(""); // recherche value clavier
+  const [totalResults, setTotalResults] = useState(0); // nombre d'events trouvés
 
+
+  // pour bouton next page
   function goNextPage() {
-    const newPage = page + 1;
-    const newOffset = newPage * limit - limit;
-    setPage(newPage);
-    setOffset(newOffset);
+    const newPage = page + 1; // on ajoute un a page actuel
+    const newOffset = newPage * limit - limit; // calcul de l'offset en fonction de la page
+    setPage(newPage); // on recatualise page
+    setOffset(newOffset); // on reactualise offset
   }
 
+  // pour bouton prev IDEM en negatif
   function goPrevPage() {
     if (page > 1) {
       const newPage = page - 1;
@@ -35,15 +38,20 @@ function App() {
           setOffset(0);
           setPage(1);
         }}
+        // on passe a search bar query en props 
+        // a chaque reactualisation de query tu actualise query 
+        // + tu reviens a page 1 avec un offset de 0 (normal pour une recherche)
       />
 
+
+    {/* ici petite balise pour indiquer le nombre d'evenements trouvés*/}
       {totalResults > 0 && (
         <p className="text-gray-600 mb-2">
           {totalResults} évènement{totalResults > 1 ? "s" : ""} trouvé
         </p>
       )}
 
-      {/* ✅ On appelle maintenant EventList */}
+      {/* On appelle maintenant EventList */}
       <EventList
         offset={offset}
         limit={limit}
